474b9e8016e2b686df8d85abb1b41eb3a5fe166e
Anthony Sá Reis <anthonysareis11@gmail.com>
2022-10-21 10:54:38 -0300

    initial commit

---
+++ src/components/Board.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "./Box";
+import "./Board.css";
+export const Board = ({board, onClick}) =>{
+    return(
+        <div className="board"> 
+        {board.map((value,idx)=>{
+            return <Box value = {value} onClick={()=> value===null && onClick(idx)}/> // don't choose twice
+        })}
+       
+        </div>
+    );
+}
---
+++ src/components/ResetButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ResetButton.css"
+
+export const ResetButton = ({resetBoard}) =>{
+    return(
+        <button className="reset-btn" onClick={resetBoard}>Reset</button>
+    );
+}
